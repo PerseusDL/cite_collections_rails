5662eeee4e196be3d2633a97510392f519f1cb6d
annakrohn <akkrohn@gmail.com>
2014-10-02 10:58:28 -0400

    corrected rails routing to account for apache proxy

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 CiteCollections::Application.initialize!
+
+ENV['RAILS_RELATIVE_URL_ROOT'] = '/cite-collections'
