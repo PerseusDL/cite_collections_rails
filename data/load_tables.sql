ba488295bb5e4eec122f52b9e6592bd854e29562
annakrohn <akkrohn@gmail.com>
2014-11-12 12:10:50 -0500

    updating sql schema and latest info from fusion tables

--- data/load_tables.sql
+++ data/load_tables.sql
@@ -8,12 +8,12 @@ lines terminated by '\n'
 load data local infile '/Users/ada/cite_collections_rails/data/Perseus Work Collection.csv' into table cite_tables.works fields terminated by ','
 enclosed by '"'
 lines terminated by '\n'
-(urn, work, title_eng, orig_lang, notes, urn_status, created_by, edited_by);
+(urn, work, title_eng, orig_lang, notes, urn_status, redirect_to, created_by, edited_by);
 
 load data local infile '/Users/ada/cite_collections_rails/data/Perseus Textgroup Collection.csv' into table textgroups fields terminated by ','
 enclosed by '"'
 lines terminated by '\n'
-(urn, textgroup, groupname_eng, has_mads, mads_possible, notes, urn_status, created_by, edited_by);
+(urn, textgroup, groupname_eng, has_mads, mads_possible, notes, urn_status, redirect_to, created_by, edited_by);
 
 load data local infile '/Users/ada/cite_collections_rails/data/Perseus Version Collection.csv' into table versions fields terminated by ','
 enclosed by '"'
